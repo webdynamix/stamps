b3c81d62a33be6db5763630b54c2d15d95d07071
webdynamix <vale@webdynamix.com>
2017-06-06 11:24:45 -0700

    Update bundle.js

--- dist/bundle.js
+++ dist/bundle.js
@@ -68,51 +68,48 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
+
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 /*
   reusable for attaching events
 */
-const bindEvent = (element, event, delegate) => {
+var bindEvent = exports.bindEvent = function bindEvent(element, event, delegate) {
   if (typeof window.event != 'undefined' && element.attachEvent) element.attachEvent('on' + event, delegate);else element.addEventListener(event, delegate, false);
 };
-/* harmony export (immutable) */ __webpack_exports__["b"] = bindEvent;
-
 /*
   Similar to jQuery's $( document ).ready()
 */
-const onDocumentReady = callback => {
-  bindEvent(document, 'readystatechange', () => {
+var onDocumentReady = exports.onDocumentReady = function onDocumentReady(callback) {
+  bindEvent(document, 'readystatechange', function () {
     if (document.readyState !== "complete") return true;
 
     callback();
   });
 };
-/* harmony export (immutable) */ __webpack_exports__["a"] = onDocumentReady;
-
 
 /*
   preventing events moving to their next target (bubbling)
 */
-const preventBubbling = event => {
+var preventBubbling = exports.preventBubbling = function preventBubbling(event) {
   if (event.preventDefault) event.preventDefault();
   if (event.stopPropagation) event.stopPropagation();else event.cancelBubble = true;
 };
-/* harmony export (immutable) */ __webpack_exports__["e"] = preventBubbling;
 
-
-const getRandom = (min, max) => {
+var getRandom = exports.getRandom = function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
-/* harmony export (immutable) */ __webpack_exports__["d"] = getRandom;
 
+var appendSelectOptions = exports.appendSelectOptions = function appendSelectOptions(selectbox, limit) {
+  var random = getRandom(1, limit);
 
-const appendSelectOptions = (selectbox, limit) => {
-  const random = getRandom(1, limit);
-
-  for (let i = 1; i <= limit; i++) {
-    const option = document.createElement('option');
+  for (var i = 1; i <= limit; i++) {
+    var option = document.createElement('option');
     option.text = i;
     option.value = i;
     if (i === random) option.selected = true;
@@ -120,26 +117,29 @@ const appendSelectOptions = (selectbox, limit) => {
     selectbox.appendChild(option);
   }
 };
-/* harmony export (immutable) */ __webpack_exports__["f"] = appendSelectOptions;
-
 /*
   *Toggles .expanded on #navCollapse for mobile nav menu
 */
-const navToggle = event => {
-  const el = document.getElementById('navCollapse');
+var navToggle = exports.navToggle = function navToggle(event) {
+  var el = document.getElementById('navCollapse');
   if (el.classList.value.indexOf('expanded') === -1) el.classList.add('expanded');else el.classList.remove('expanded');
 };
-/* harmony export (immutable) */ __webpack_exports__["c"] = navToggle;
-
 
 /***/ }),
 /* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__helpers__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__cart__ = __webpack_require__(3);
+
+
+var _helpers = __webpack_require__(0);
+
+var _cart = __webpack_require__(3);
+
+var _cart2 = _interopRequireDefault(_cart);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /*
   *! I'm avoiding using any javascript frameworks. I didn't see a need here so decided
   to write from scratch.
@@ -147,18 +147,15 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
   *! I'm using the HTML5 Drag and Drop interface.
     * Supported by most major browsers (not mobile).
 */
+(0, _helpers.onDocumentReady)(function () {
 
-
-
-__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["a" /* onDocumentReady */])(() => {
-
-  const MyCart = new __WEBPACK_IMPORTED_MODULE_1__cart__["a" /* default */]({
+  var MyCart = new _cart2.default({
     cart: '#cart',
     products: 'ul#products li',
     optionsLimit: 20
   });
 
-  __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["b" /* bindEvent */])(document.getElementById('navCollapsebtn'), 'click', __WEBPACK_IMPORTED_MODULE_0__helpers__["c" /* navToggle */]);
+  (0, _helpers.bindEvent)(document.getElementById('navCollapsebtn'), 'click', _helpers.navToggle);
 });
 
 /***/ }),
@@ -169,16 +166,33 @@ __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["a" /* onDocumentRe
 
 /***/ }),
 /* 3 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__helpers__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__product__ = __webpack_require__(4);
 
 
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-class Cart {
-  constructor(props) {
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _helpers = __webpack_require__(0);
+
+var _product = __webpack_require__(4);
+
+var _product2 = _interopRequireDefault(_product);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Cart = function () {
+  function Cart(props) {
+    var _this = this;
+
+    _classCallCheck(this, Cart);
+
     /*
       * Self invoking, no need to call constructor
       * takes props from when the object was initiated
@@ -194,154 +208,189 @@ class Cart {
     this.count = this.cart.querySelector('.count');
     this.amount = this.cart.querySelector('.amount');
 
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["b" /* bindEvent */])(this.cart, 'drop', event => this.onDrop(event));
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["b" /* bindEvent */])(this.cart, 'dragover', event => this.onOver(event));
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["b" /* bindEvent */])(this.cart, 'dragleave', event => this.onDragLeave(event));
+    (0, _helpers.bindEvent)(this.cart, 'drop', function (event) {
+      return _this.onDrop(event);
+    });
+    (0, _helpers.bindEvent)(this.cart, 'dragover', function (event) {
+      return _this.onOver(event);
+    });
+    (0, _helpers.bindEvent)(this.cart, 'dragleave', function (event) {
+      return _this.onDragLeave(event);
+    });
 
     this.products(props.products);
   }
 
-  products(items) {
-    /*
-      * For each product element create a new Product object
-    */
-    const products = document.querySelectorAll(items);
+  _createClass(Cart, [{
+    key: 'products',
+    value: function products(items) {
+      /*
+        * For each product element create a new Product object
+      */
+      var products = document.querySelectorAll(items);
 
-    for (var i = 0; i < products.length; i++) {
-      const MyProduct = new __WEBPACK_IMPORTED_MODULE_1__product__["a" /* default */]({
-        element: products[i],
-        optionsLimit: this.state.optionsLimit
-      });
-    }
-  }
-
-  updateCart() {
-    // update ui feedback
-    this.count.innerText = this.state.count;
-    this.amount.innerText = this.state.amount.toFixed(2);
-  }
-
-  addToCart(item, id) {
-    // access price and current selection from dataset
-    const currentSelection = item.dataset.selections;
-    const price = item.getAttribute('data-price');
-    const total = Number(price) * currentSelection;
-    // add to current values
-    this.state.amount += total;
-    this.state.count += Number(currentSelection);
-  }
-
-  resetOption(item) {
-    let option;
-    const select = item.querySelector('select');
-    const random = __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["d" /* getRandom */])(1, 20);
-    item.dataset.selections = random;
-
-    // loop thu options and match option value to random
-    for (let i = 0; i < select.options.length; i++) {
-      option = select.options[i];
-      if (Number(option.value) === random) {
-        option.selected = true;
-        return;
+      for (var i = 0; i < products.length; i++) {
+        var MyProduct = new _product2.default({
+          element: products[i],
+          optionsLimit: this.state.optionsLimit
+        });
       }
     }
-  }
+  }, {
+    key: 'updateCart',
+    value: function updateCart() {
+      // update ui feedback
+      this.count.innerText = this.state.count;
+      this.amount.innerText = this.state.amount.toFixed(2);
+    }
+  }, {
+    key: 'addToCart',
+    value: function addToCart(item, id) {
+      // access price and current selection from dataset
+      var currentSelection = item.dataset.selections;
+      var price = item.getAttribute('data-price');
+      var total = Number(price) * currentSelection;
+      // add to current values
+      this.state.amount += total;
+      this.state.count += Number(currentSelection);
+    }
+  }, {
+    key: 'resetOption',
+    value: function resetOption(item) {
+      var option = void 0;
+      var select = item.querySelector('select');
+      var random = (0, _helpers.getRandom)(1, 20);
+      item.dataset.selections = random;
 
-  onDrop(event) {
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["e" /* preventBubbling */])(event);
-    // Retrieve product id assigned
-    const id = event.dataTransfer.getData("Id");
-    const item = document.getElementById(id);
-    this.addToCart(item, id);
-    // update values in UI
-    this.updateCart();
+      // loop thu options and match option value to random
+      for (var i = 0; i < select.options.length; i++) {
+        option = select.options[i];
+        if (Number(option.value) === random) {
+          option.selected = true;
+          return;
+        }
+      }
+    }
+  }, {
+    key: 'onDrop',
+    value: function onDrop(event) {
+      (0, _helpers.preventBubbling)(event);
+      // Retrieve product id assigned
+      var id = event.dataTransfer.getData("Id");
+      var item = document.getElementById(id);
+      this.addToCart(item, id);
+      // update values in UI
+      this.updateCart();
 
-    this.resetOption(item);
-    this.cart.classList.remove('active');
-    return false;
-  }
+      this.resetOption(item);
+      this.cart.classList.remove('active');
+      return false;
+    }
+  }, {
+    key: 'onOver',
+    value: function onOver(event) {
+      (0, _helpers.preventBubbling)(event);
+      if (this.cart.classList.value.indexOf('active') === -1) this.cart.classList.add('active');
+      return false;
+    }
+  }, {
+    key: 'onDragLeave',
+    value: function onDragLeave(e) {
+      if (this.cart.classList.value.indexOf('active') > -1) this.cart.classList.remove('active');
+      return false;
+    }
+  }]);
 
-  onOver(event) {
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["e" /* preventBubbling */])(event);
-    if (this.cart.classList.value.indexOf('active') === -1) this.cart.classList.add('active');
-    return false;
-  }
+  return Cart;
+}();
 
-  onDragLeave(e) {
-    if (this.cart.classList.value.indexOf('active') > -1) this.cart.classList.remove('active');
-    return false;
-  }
-
-}
-/* harmony export (immutable) */ __webpack_exports__["a"] = Cart;
-
+exports.default = Cart;
 
 /***/ }),
 /* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__helpers__ = __webpack_require__(0);
 
 
-class Product {
-  constructor(props) {
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _helpers = __webpack_require__(0);
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Product = function () {
+  function Product(props) {
+    _classCallCheck(this, Product);
+
     this.product = props.element;
-    const product = props.element;
+    var product = props.element;
 
     // make product draggable
     product.setAttribute("draggable", "true");
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["b" /* bindEvent */])(product, 'dragstart', this.onDrag);
+    (0, _helpers.bindEvent)(product, 'dragstart', this.onDrag);
 
-    const image = this.product.querySelector('img');
+    var image = this.product.querySelector('img');
     if (!!image) image.dataset.id = this.product.id;
 
     this.addSelectBoxOptions(props.optionsLimit);
   }
 
-  onOptionChange(e) {
-    // update current selection in product's dataset
-    const currentSelection = e.target.options[e.target.selectedIndex].value;
-    this.product.dataset.selections = currentSelection;
-  }
+  _createClass(Product, [{
+    key: "onOptionChange",
+    value: function onOptionChange(e) {
+      // update current selection in product's dataset
+      var currentSelection = e.target.options[e.target.selectedIndex].value;
+      this.product.dataset.selections = currentSelection;
+    }
+  }, {
+    key: "addSelectBoxOptions",
+    value: function addSelectBoxOptions(optionsLimit) {
+      var _this = this;
 
-  addSelectBoxOptions(optionsLimit) {
-    const selectBox = this.product.querySelector('.selectBox');
-    const sbox = selectBox.querySelector('select');
-    const arrow = document.createElement('i');
-    // helper to append options with a random selection
-    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__helpers__["f" /* appendSelectOptions */])(sbox, optionsLimit);
+      var selectBox = this.product.querySelector('.selectBox');
+      var sbox = selectBox.querySelector('select');
+      var arrow = document.createElement('i');
+      // helper to append options with a random selection
+      (0, _helpers.appendSelectOptions)(sbox, optionsLimit);
 
-    const currentSelection = sbox.options[sbox.selectedIndex].value;
+      var currentSelection = sbox.options[sbox.selectedIndex].value;
 
-    sbox.onchange = e => {
-      this.onOptionChange(e);
-    };
-    // assign initial selection(random) to product's dataset
-    this.product.dataset.selections = currentSelection;
-    // appeding i element to be styled as arrow
-    selectBox.appendChild(arrow);
-  }
+      sbox.onchange = function (e) {
+        _this.onOptionChange(e);
+      };
+      // assign initial selection(random) to product's dataset
+      this.product.dataset.selections = currentSelection;
+      // appeding i element to be styled as arrow
+      selectBox.appendChild(arrow);
+    }
+  }, {
+    key: "onDrag",
+    value: function onDrag(event) {
+      /*
+        using event dataTransfer properties from
+        HTML5's DnD api.
+      */
+      event.dataTransfer.effectAllowed = "move";
+      event.dataTransfer.dropEffect = "move";
+      var target = event.target || event.srcElement;
+      // I'm assigning the product image as the drag image
+      // Making sure the cursor is at the top right 10% of the image.
+      event.dataTransfer.setDragImage(target, target.width * .9, target.width * .1);
+      // assigning the product id as data property
+      // to be retrieved if product is dropped in cart
+      event.dataTransfer.setData('Id', target.dataset.id);
+    }
+  }]);
 
-  onDrag(event) {
-    /*
-      using event dataTransfer properties from
-      HTML5's DnD api.
-    */
-    event.dataTransfer.effectAllowed = "move";
-    event.dataTransfer.dropEffect = "move";
-    const target = event.target || event.srcElement;
-    // I'm assigning the product image as the drag image
-    // Making sure the cursor is at the top right 10% of the image.
-    event.dataTransfer.setDragImage(target, target.width * .9, target.width * .1);
-    // assigning the product id as data property
-    // to be retrieved if product is dropped in cart
-    event.dataTransfer.setData('Id', target.dataset.id);
-  }
+  return Product;
+}();
 
-}
-/* harmony export (immutable) */ __webpack_exports__["a"] = Product;
-
+exports.default = Product;
 
 /***/ }),
 /* 5 */
